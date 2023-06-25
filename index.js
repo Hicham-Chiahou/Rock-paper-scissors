c2e3ca7bdf842503152375ff219f4477479d462b
hicham chiahou <hicham.chiahou@gmail.com>
2023-06-25 11:35:14 +0100

    the getComputerChoice return random words
        this function now return randomly 3 words 'Rocks', 'Paper'
        and  'Scissors'.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function getComputerChoice(){
-    let e = Math.random()
-    console.log(e)
+function getComputerChoice() {
+
+  let e = Math.floor(Math.random() * 3);
+  if (e === 0) {
+    console.log("Rock");
+  } else if (e === 1) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+  
 }
 
-getComputerChoice()
+getComputerChoice();
