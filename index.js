20584bb11b0d8a1bd5b12bd9e2739a21305cb728
hicham chiahou <hicham.chiahou@gmail.com>
2023-06-28 13:10:53 +0100

    a Readme.md file day tracking time

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let playerSelection = "Rock";
 let computerChoice = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
+console.log("player select: " + playerSelection);
+console.log("computer select: " + computerChoice);
+
+
 
 function getComputerChoice() {
   let e = Math.floor(Math.random() * 3);
   if (e === 0) {
     return "Rock";
-
   } else if (e === 1) {
     return "Paper";
-
   } else {
-    return "Scissor";
+    return "Scissors";
   }
 }
 
@@ -35,14 +40,47 @@ function playRound(playerSelection, ComputerSelection) {
 
   /* the playerSelection Scissors option cases */
   if (playerSelection === "Scissors" && ComputerSelection === "Rock") {
-    return 'You Lose! Scissors beats Rock'
+    return "You Lose! Scissors beats Rock";
   } else if (playerSelection === "Scissors" && ComputerSelection === "Paper") {
-    return 'You Won! Scissors beats Paper'
+    return "You Won! Scissors beats Paper";
   } else if (
-    playerSelection === "Scissors" && ComputerSelection === "Scissors"
+    playerSelection === "Scissors" &&
+    ComputerSelection === "Scissors"
   ) {
-    return 'You are evan! '
+    return "You are evan! ";
   }
 }
 
-console.log(playRound(playerSelection, computerChoice))
+let round = playRound(playerSelection, computerChoice);
+
+
+function playGame() {
+  
+  if (round === "You Won ! Paper beats Rock") {
+    playerScore++;
+    
+
+  }
+  if (round === "You Won! Rock beats Scissors") {
+    playerScore++;
+  }
+  if (round === "You Won! Scissors beats Paper") {
+    playerScore++;
+  }
+  if (round === "You Lose! Paper beats Rock") {
+    computerScore++;
+  }
+  if (round === "You Lose! Scissors beats Paper") {
+    computerScore++;
+  }
+  if (round === "You Lose! Scissors beats Rock") {
+    computerScore++;
+  }
+
+
+
+}
+
+console.log("player score: " + playerScore);
+console.log("computer score: " + computerScore);
+
